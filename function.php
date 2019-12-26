baf4187b6cc445b53de7b28692481b0a7df14076
yutasb <shibamoto.yuta@gmail.com>
2019-12-26 22:34:15 +0900

    ok

--- function.php
+++ function.php
@@ -122,21 +122,37 @@
         }
     }
 
+    // function dbConnect()
+    // {
+    //     $dsn = 'mysql:dbname=superp_view;host=localhost;charset=utf8';
+    //     $user = 'root';
+    //     $password = 'root';
+    //     $options = array(
+    //         // SQL実行失敗時にはエラーコードのみ設定
+    //         PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+    //         // デフォルトフェッチモードを連想配列形式に設定
+    //         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    //         // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
+    //         // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
+    //         PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    //     );
+    //     // PDOオブジェクト生成（DBへ接続）
+    //     $dbh = new PDO($dsn, $user, $password, $options);
+    //     return $dbh;
+    // }
+
     function dbConnect()
     {
-        $dsn = 'mysql:dbname=superp_view;host=localhost;charset=utf8';
-        $user = 'root';
-        $password = 'root';
+        $db = parse_url($_SERVER['mysql://b842d53109bb1c:366265b3@us-cdbr-iron-east-05.cleardb.net/heroku_b74f4a3d8a0f14c?reconnect=true']);
+        $db['heroku_b74f4a3d8a0f14c'] = ltrim($db['path'], '/');
+        $dsn = "mysql:host={$db['us-cdbr-iron-east-05.cleardb.net']};dbname={$db['heroku_b74f4a3d8a0f14c']};charset=utf8";
+        $user = $db['b842d53109bb1c'];
+        $password = $db['366265b3'];
         $options = array(
-            // SQL実行失敗時にはエラーコードのみ設定
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
-            // デフォルトフェッチモードを連想配列形式に設定
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
-            // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
             PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
         );
-        // PDOオブジェクト生成（DBへ接続）
         $dbh = new PDO($dsn, $user, $password, $options);
         return $dbh;
     }
