39679442981ee5a4c70e0e912e3106174bb4e20c
yutasb <shibamoto.yuta@gmail.com>
2019-12-30 17:49:16 +0900

    re_insert

--- function.php
+++ function.php
@@ -4,8 +4,14 @@
     ini_set('error_log', 'php.log');
 
     ini_set('session.save_handler', 'memcached');
-    ini_set('session.save_path', 'PERSISTENT=pool ' . getenv('MEMCACHIER_SERVERS'));
-    ini_set('memcached.sess_binary', 1);
+    ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
+    if (version_compare(phpversion('memcached'), '3', '>=')) {
+        ini_set('memcached.sess_persistent', 1);
+        ini_set('memcached.sess_binary_protocol', 1);
+    } else {
+        ini_set('session.save_path', 'PERSISTENT=myapp_session ' . ini_get('session.save_path'));
+        ini_set('memcached.sess_binary', 1);
+    }
     ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
     ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
 
